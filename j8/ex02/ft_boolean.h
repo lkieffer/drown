c000d45d094da5f65694f89589b0fdb45d1e5cf4
Lucas KIEFFER <lkieffer@e3r7p15.42.fr>
2017-08-20 07:59:52 +0200

    ex02

---
+++ j8/ex02/ft_boolean.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lkieffer <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/08/20 07:34:08 by lkieffer          #+#    #+#             */
+/*   Updated: 2017/08/20 07:58:26 by lkieffer         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef __FT_BOOLEAN_H
+# define __FT_BOOLEAN_H
+
+# include <unistd.h>
+
+typedef int	t_bool;
+# define EVEN(x) x % 2 == 0
+# define TRUE 1
+# define FALSE 0
+# define EVEN_MSG "I have an even number of arguments."
+# define ODD_MSG "I have an odd number of arguments."
+# define SUCCES 0
+
+#endif
